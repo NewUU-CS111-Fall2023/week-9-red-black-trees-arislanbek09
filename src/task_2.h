73eff961227f0aaca7fb7869f62a2f0a5c2c5b64
Arislanbek Kalbaev <127713555+arislanbek09@users.noreply.github.com>
2023-12-13 18:47:44 +0500

    Create task_2.h

---
+++ src/task_2.h
@@ -0,0 +1,203 @@
+#include <iostream>
+
+enum Color { RED, BLACK };
+
+class Node {
+public:
+    int data;
+    Color color;
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    Node(int value, Color c = RED, Node* p = nullptr, Node* l = nullptr, Node* r = nullptr)
+            : data(value), color(c), parent(p), left(l), right(r) {}
+};
+
+class RedBlackTree {
+private:
+    Node* root;
+
+    void rotateLeft(Node* x);
+    void rotateRight(Node* y);
+    void leftRotate(Node* x);
+    void rightRotate(Node* y);
+    void fixInsertion(Node* z);
+    void printTree(Node* root, int space);
+
+public:
+    RedBlackTree() : root(nullptr) {}
+
+    void Insert(int data);
+    void printTree();
+};
+
+void RedBlackTree::rotateLeft(Node* x) {
+    Node* y = x->right;
+    x->right = y->left;
+    if (y->left != nullptr) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == nullptr) {
+        root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+
+void RedBlackTree::rotateRight(Node* y) {
+    Node* x = y->left;
+    y->left = x->right;
+    if (x->right != nullptr) {
+        x->right->parent = y;
+    }
+    x->parent = y->parent;
+    if (y->parent == nullptr) {
+        root = x;
+    } else if (y == y->parent->left) {
+        y->parent->left = x;
+    } else {
+        y->parent->right = x;
+    }
+    x->right = y;
+    y->parent = x;
+}
+
+void RedBlackTree::leftRotate(Node* x) {
+    if (x == nullptr || x->right == nullptr)
+        return;
+
+    Node* y = x->right;
+    x->right = y->left;
+    if (y->left != nullptr) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == nullptr) {
+        root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+
+void RedBlackTree::rightRotate(Node* y) {
+    if (y == nullptr || y->left == nullptr)
+        return;
+
+    Node* x = y->left;
+    y->left = x->right;
+    if (x->right != nullptr) {
+        x->right->parent = y;
+    }
+    x->parent = y->parent;
+    if (y->parent == nullptr) {
+        root = x;
+    } else if (y == y->parent->left) {
+        y->parent->left = x;
+    } else {
+        y->parent->right = x;
+    }
+    x->right = y;
+    y->parent = x;
+}
+
+void RedBlackTree::fixInsertion(Node* z) {
+    while (z != root && z->parent->color == RED) {
+        if (z->parent == z->parent->parent->left) {
+            Node* y = z->parent->parent->right;
+            if (y != nullptr && y->color == RED) {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    rotateLeft(z);
+                }
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rotateRight(z->parent->parent);
+            }
+        } else {
+            Node* y = z->parent->parent->left;
+            if (y != nullptr && y->color == RED) {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    rotateRight(z);
+                }
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                rotateLeft(z->parent->parent);
+            }
+        }
+    }
+    root->color = BLACK;
+}
+
+void RedBlackTree::Insert(int data) {
+    Node* z = new Node(data);
+    Node* y = nullptr;
+    Node* x = root;
+
+    while (x != nullptr) {
+        y = x;
+        if (z->data < x->data) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+
+    z->parent = y;
+    if (y == nullptr) {
+        root = z;
+    } else if (z->data < y->data) {
+        y->left = z;
+    } else {
+        y->right = z;
+    }
+
+    fixInsertion(z);
+}
+
+void RedBlackTree::printTree() {
+    printTree(root, 0);
+}
+
+void RedBlackTree::printTree(Node* root, int space) {
+    if (root == nullptr) {
+        return;
+    }
+
+    space += 4;
+
+    printTree(root->right, space);
+
+    std::cout << std::endl;
+    for (int i = 4; i < space; i++) {
+        std::cout << " ";
+    }
+
+    if (root->color == RED) {
+        std::cout << root->data << "(RED)";
+    } else {
+        std::cout << root->data << "(BLACK)";
+    }
+
+    printTree(root->left, space);
+}
